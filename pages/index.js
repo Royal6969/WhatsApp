6de1302c7071160a90524af85dd49be12ce35274
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-06 17:42:24 +0200

    building sidebar component - header

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar';
 //import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+//import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>WhatsApp 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Lets build WhatsApp 2.0</h1>
-      {/* npm install --save styled-components */}
+      <Sidebar />
+      
     </div>
   );
 }
