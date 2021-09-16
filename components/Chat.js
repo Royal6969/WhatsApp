36231430d61f8a20c7fdaf2ca1b25ddfec129b5c
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-16 18:57:19 +0200

    building getEmailRecipient function - create utils folder - get email and photoURL from users

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,31 @@
 import { Avatar } from '@material-ui/core';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollection } from 'react-firebase-hooks/firestore';
 import styled from 'styled-components';
+import { auth, db } from '../firebase';
+import getRecipientEmail from '../utils/getRecipientEmail';
 
 function Chat({ id, users }) { // props distructured
+
+    const [user] = useAuthState(auth);
+
+    const [recipientSnapshot] = useCollection(
+        db.collection('users').where('email', '==', getRecipientEmail(users, user))
+    );
+
+    const recipient = recipientSnapshot?.docs?.[0]?.data();
+
+    const recipientEmail = getRecipientEmail(users, user);
+
     return (
         <Container>
+            {recipient ? (
+                <UserAvatar src={recipient?.photoURL} /> 
+            ) : (
+                <UserAvatar>{recipientEmail[0]}</UserAvatar>
+            )}
             <UserAvatar />
-            <p>Recipient Email</p>
+            <p>{recipientEmail}</p>
         </Container>
     );
 }
