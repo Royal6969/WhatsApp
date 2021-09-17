d26c6112fcf7aa9c41edca6d6ad1e902f566c2cd
Royal6969 <sergiodiazcampos@gmail.com>
2021-09-17 19:05:49 +0200

    building [id] component - useRouter, SSR, useAuthState

--- components/Chat.js
+++ components/Chat.js
@@ -1,4 +1,5 @@
 import { Avatar } from '@material-ui/core';
+import { useRouter } from 'next/dist/client/router';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import styled from 'styled-components';
@@ -7,18 +8,24 @@ import getRecipientEmail from '../utils/getRecipientEmail';
 
 function Chat({ id, users }) { // props distructured
 
+    const router = useRouter();
+
     const [user] = useAuthState(auth);
 
     const [recipientSnapshot] = useCollection(
         db.collection('users').where('email', '==', getRecipientEmail(users, user))
     );
 
+    const enterChat = () => {
+        router.push(`/chat/${id}`);
+    };
+
     const recipient = recipientSnapshot?.docs?.[0]?.data();
 
     const recipientEmail = getRecipientEmail(users, user);
 
     return (
-        <Container>
+        <Container onClick={enterChat}>
             {recipient ? (
                 <UserAvatar src={recipient?.photoURL} /> 
             ) : (
